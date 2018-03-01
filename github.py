a28c5ceb1e26a0095a9e3ab34c266245d44efd42
Victor Lau <lau.victor.w@gmail.com>
2018-02-28 22:45:31 -0800

    working solution to get contributors

--- github.py
+++ github.py
@@ -4,21 +4,19 @@ class GitHubFetcher():
 
   def __init__(self, *argv):
     self.endpoint = 'https://api.github.com/repos/vinay-shipt/de-take-home'
-    self.contributor_url = self.request(self.endpoint)['contributors_url']
-    self.contributors = self.request(self.contributor_url)
+    self.contributor_url = self.githubfetch(self.endpoint)['contributors_url']
+    self.contributors = self.githubfetch(self.contributor_url)
     self.get_contributors(self.contributors)
 
-  def request(self, endpoint):
-    response = requests.get(self.endpoint)
+  def githubfetch(self, endpoint):
+    response = requests.get(endpoint)
     text_object = json.loads(response.text)
     return text_object
 
   def get_contributors(self, contributors):
-    if type(contributors) is dict:
-      print(contributors['login'])
-    else:
-      for contributor in contributors:
-        print(contributor['login'])
+    for contributor in contributors:
+      #Could Write to File Here or save to DB?
+      print(contributor['login'])
 
 if __name__ == '__main__':
   GitHubFetcher()
